c89bcce7f7e77804c28dc4ef27cabda21d4f6192
Hussein Jaber <hujaber@me.com>
2017-08-16 15:57:00 +0300

    Simple Implementation of iOS native QR code reader

--- QRReader/ViewController.m
+++ QRReader/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+@property (weak, nonatomic) IBOutlet UIView *cameraView;
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+@property (nonatomic) BOOL isReading;
+
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+@property (weak, nonatomic) IBOutlet UIButton *rescanButton;
 
 @end
 
@@ -16,14 +24,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.isReading = NO;
+    self.captureSession = nil;
+    [self startStopReading];
+    self.rescanButton.hidden = YES;
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)startStopReading {
+    if (!self.isReading) {
+        [self startReading];
+    } else {
+        [self stopReading];
+    }
+    self.isReading = !self.isReading;
 }
 
+- (BOOL)startReading {
+    self.rescanButton.hidden = YES;
+    NSError *error;
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    if (!input) {
+        NSLog(@"%@", error.localizedDescription);
+        return NO;
+    }
+    self.captureSession = [[AVCaptureSession alloc] init];
+    [self.captureSession addInput:input];
+    AVCaptureMetadataOutput *captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [_captureSession addOutput:captureMetadataOutput];
+
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetadataOutput setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
+
+    self.videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    [self.videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [self.videoPreviewLayer setFrame:self.cameraView.layer.bounds];
+    [self.cameraView.layer addSublayer:self.videoPreviewLayer];
+    [self.captureSession startRunning];
+
+    return YES;
+}
+
+- (void)stopReading {
+    [self.captureSession stopRunning];
+    self.captureSession = nil;
+    [self.videoPreviewLayer removeFromSuperlayer];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+    if (metadataObjects.count) {
+        AVMetadataMachineReadableCodeObject *metaObject = [metadataObjects firstObject];
+        if ([metaObject.type isEqualToString:AVMetadataObjectTypeQRCode]) {
+            __weak ViewController *weakSelf = self;
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [weakSelf processQRCode:metaObject];
+            });
+        }
+    }
+}
+
+- (void)processQRCode:(AVMetadataMachineReadableCodeObject *)object {
+    self.infoLabel.text = object.stringValue;
+    [self stopReading];
+    self.isReading = NO;
+    self.rescanButton.hidden = NO;
+}
+
+- (IBAction)rescanAction:(UIButton *)sender {
+    if (!self.isReading) {
+        [self startReading];
+    }
+}
 
 @end
